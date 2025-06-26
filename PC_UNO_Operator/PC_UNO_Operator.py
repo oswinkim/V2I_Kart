a52be65cd47380e5758a4c1256e61b75bdac8319
DESKTOP-CKMEEMI\user <oswinkim05@gmail.com>
2025-06-26 16:52:50 +0900

    #6 연산컴퓨터 클래스화 및 AHRS, 컬러값 송수신 추가

--- PC_UNO_Operator/PC_UNO_Operator.py
+++ PC_UNO_Operator/PC_UNO_Operator.py
@@ -2,80 +2,131 @@ import socket
 import select
 
 WorldSockets=[]
+keys_move=["w","a","s","d"]
+
+class Common:
+    num = 0
+
+    def __init__(self, Ip = "", Send_port = "", Rev_port = ""):
+        print("[소캣설정 시작]")
+
+
+        Common.num += 1
+
+        self.Priority = Common.num
+        print(f"우선순위: {self.Priority}")
+
+        if not Ip:
+            Ip = input("ip가 설정되지 않았습니다.ex) 0.0.0.0: ")
+        self.Ip = Ip
+        print(f"ip: {self.Ip}")
+
+        if not Send_port:
+            Send_port = input("송신포트가 설정되지 않았습니다.ex) 5000: ")
+        self.Send_port = int(Send_port)
+        print(f"송신포트: {self.Send_port}")
+
+        if not Rev_port:
+            Rev_port = input("수신포트가 설정되지 않았습니다.ex) 5001: ")
+        self.Rev_port = int(Rev_port)
+        print(f"수신포트: {self.Rev_port}\n")
+
+    def Bind_listen(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket.bind(("", self.Send_port))
+
+        WorldSockets.append(self.socket)
+
+class Player(Common):
+    def __init__(self, Name="", Ip="", Send_port="", Rev_port=""):
+        print("[Player 설정시작]")
+        self.Type = "Player"
+        print(f"객체 타입: {self.Type}")
+
+        if not Name:
+            Name = input("플레이어의 이름이 설정되지 않았습니다.ex) 다오: ")
+        self.Name=Name
+        print(f"플레이어 이름: {self.Name}")
+
+        super().__init__(Ip, Send_port, Rev_port)
+
+        super().Bind_listen()
+
+class kart(Common):
+    def __init__(self, Name="", Ip="", Send_port="", Rev_port=""):
+        print("[Kart 설정시작]")
+        self.Type = "Kart"
+        print(f"객체 타입: {self.Type}")
+
+        if not Name:
+            Name = input("카트의 이름이 설정되지 않았습니다.ex) 코튼: ")
+        self.Name=Name
+        print(f"카트 이름: {self.Name}")
+
+        super().__init__(Ip, Send_port, Rev_port)
+
+        super().Bind_listen()
+
+class Infra(Common):
+    def __init__(self, Name="", Ip="", Send_port="", Rev_port=""):
+        print("[Infra 작성시작]")
+        self.Type = "Infra"
+        print(f"객체 타입: {self.Type}")
+
+        if not Name:
+            Name = input("인프라의 이름이 설정되지 않았습니다.ex) 신호등1: ")
+        self.Name=Name
+        print(f"인프라 이름: {self.Name}")
+
+        super().__init__(Ip, Send_port, Rev_port)
+
+        super().Bind_listen()
 
 class User:
-    def __init__(self, Name='', Priority=0, EspIp="", EspRecvPort=0, EspSendPort=0, PcIp="", PcRecvPort=0, PcSendPort=0):
-        print("[초기설정시작]")
-        if Name == '':
-            Name = input("이름이 설정되지 않았습니다.ex) 다오")
-        self.Name = Name
-        print(f"이름: {self.Name}")
+    def __init__(self,
+                 User_Name="",
+                 Name_kart="", Ip_kart="", Send_port_kart="", Rev_port_kart="",
+                 Name_player="", Ip_player="", Send_port_player="", Rev_port_player=""):
+        print("-----------------------------")
+        print("[User 설정시작]")
+        self.Type = "User"
+        print(f"객체 타입: {self.Type}")
 
-        if Priority == 0:
-            Priority = input(f"{self.Name}의 우선순위를 정수로 작성해주세요.ex) 1")
-        self.Priority=Priority
-        print(f"{self.Name}의 우선순위: {self.Priority}")        
-        
-        #esp32
-        if EspIp == "":
-            EspIp = input(f"{self.Name}의 (esp32보드)ip를 작성해주세요.ex)192.168.0.0")
-        self.EspIp=EspIp
-        print(f"{self.Name}의 (esp32보드)ip: {self.EspIp}")
+        if not User_Name:
+            User_Name = input("유저 이름이 설정되지 않았습니다.ex) 세이버, 다오")
+        self.User_Name = User_Name
+        print(f"유저 이름: {self.User_Name}\n")
 
-        if EspRecvPort == 0:
-            EspRecvPort = input(f"{self.Name}의 esp32가 명령을 받을 수신포트 번호를 작성해주세요.ex)1234")
-        self.EspRecvPort=EspRecvPort
-        print(f"{self.Name}의 esp32가 명령을 받을 수신포트: {self.EspRecvPort}")        
+        self.kart = kart(Name_kart, Ip_kart, Send_port_kart, Rev_port_kart)
+        self.player = Player(Name_player, Ip_player, Send_port_player, Rev_port_player)
 
-        if EspSendPort == 0:
-            EspSendPort = input(f"{self.Name}의 esp32가 명령을 보낼 송신포트 번호를 작성해주세요.ex)1234")
-        self.EspSendPort=EspSendPort
-        print(f"{self.Name}의 esp32가 명령을 보낼 송신포트: {self.EspSendPort}")        
-        
-        #pc
-        if PcIp == "":
-            PcIp = input(f"{self.Name}의 (유저컴퓨터)ip를 작성해주세요.ex)192.168.0.0")
-        self.PcIp=PcIp
-        print(f"{self.Name}의 (유저컴퓨터)ip: {self.PcIp}")
-
-        if PcRecvPort == 0:
-            PcRecvPort = input(f"{self.Name}의 Pc 키입력을 받을 수신포트 번호를 작성해주세요.ex)1234")
-        self.PcRecvPort=PcRecvPort
-        print(f"{self.Name}의 Pc 키입력을 받을 수신포트: {self.PcRecvPort}")        
-
-        if PcSendPort == 0:
-            PcSendPort = input(f"{self.Name}의 Pc로 데이터를 보낼 송신포트 번호를 작성해주세요.ex)1234")
-        self.PcSendPort=PcSendPort
-        print(f"{self.Name}의 Pc로 데이터를 보낼 송신포트: {self.PcSendPort}\n")  
-
-        #소캣 바인딩
-        self.SockRecv_UserPc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.SockRecv_UserPc.bind(("", self.PcRecvPort))  # PC2에서 키 입력을 받을 소켓
-
-        self.SockSend_UserPc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # PC2로 온도 전송용 소켓
-
-        self.SockSend_esp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.SockSend_esp.bind(("", self.EspSendPort))  # ESP32가 온도 데이터를 보낼 소켓
-
-        self.Sockets=[self.SockRecv_UserPc, self.SockSend_esp]
-
-        WorldSockets.extend(self.Sockets)
+        print("<User 설정 완료>")
+        print("-----------------------------")
 
 
+Dao = User(
+        User_Name="almaeng",
+        Name_kart="핑크베놈", Ip_kart="192.168.191.240", Send_port_kart="4213", Rev_port_kart="4212",
+        Name_player="다오", Ip_player="192.168.191.138", Send_port_player="5005", Rev_port_player="5006"
+    )
 
-Dao = User(Name='다오', Priority=1, EspIp="192.168.0.0", EspRecvPort=4210, EspSendPort=4211, PcIp="192.168.0.0", PcRecvPort=5005, PcSendPort=5006)
-# Bazzi = User(Name='배찌', Priority=2, EspIp="192.168.0.0", EspRecvPort=4212, EspSendPort=4213, PcIp="192.168.0.0", PcRecvPort=5007, PcSendPort=5008)
+"""
+Bazzi = User(
+        User_Name="sama",
+        Name_kart="버스트", Ip_kart="128.0.0.3", Send_port_kart="7000", Rev_port_kart="7001",
+        Name_player="배찌", Ip_player="128.0.0.4", Send_port_player="8000", Rev_port_player="8001"
+    )
+"""
+
+print(Dao)
+
 macron=[Dao]
 
-# 우선순위의 정렬
-for i in range(len(macron)):
-    macron[i].Priority = i + 1
 
 print("*정상적으로 연결되지 않을 경우 네트 워크 설정을 확인하십시오.(공용 -> 개인 네트워크)")
-print("Waiting for key input from PC2 and data from ESP32...")
+print("Waiting for key input from Player and data from Kart...")
 
 while True:
-
     try:
         # select를 사용하여 먼저 오는 데이터 처리
         readable, _, _ = select.select(WorldSockets, [], [])
@@ -85,30 +136,50 @@ while True:
             msg = data.decode().strip()
 
             for i in range(len(macron)):
+                # player에서 온 키 입력 처리
                 try:
-                        # PC2에서 온 키 입력 처리
-                    if sock == macron[i].SockRecv_UserPc:
-                        print(f"Received from [{macron[i].Name}]PC: {msg}")
+                    if sock == macron[i].player.socket:
+                        print(f"Received from [{macron[i].player.Name}]PC: {msg}")
 
-                        if msg == "1":
-                            macron[i].SockSend_esp.sendto("1".encode(), (macron[i].EspIp, macron[i].EspRecvPort))  # motor ON
-                            print(f"Sent to [{macron[i].Name}]ESP: motor ON")
+                        if msg in keys_move:
+                            macron[i].kart.socket.sendto(msg.encode(), (macron[i].kart.Ip, macron[i].kart.Rev_port))  # motor ON
+                            print(f"Sent to [{macron[i].kart.Name}]ESP: {msg}")
 
-                        elif msg == "0":
-                            macron[i].SockSend_esp.sendto("0".encode(), (macron[i].EspIp, macron[i].EspRecvPort))  # motor OFF
-                            print(f"Sent to [{macron[i].Name}]ESP: motor OFF")
+                        elif msg == "i":
+                            macron[i].kart.socket.sendto("i".encode(), (macron[i].kart.Ip, macron[i].kart.Rev_port))  # motor OFF
+                            print(f"Sent to [{macron[i].kart.Name}]ESP: motor OFF")
 
                         # 추후 데이터 전송 명령어
-                        elif msg == "2":
-                            macron[i].SockSend_esp.sendto("REQ".encode(), (macron[i].EspIp, macron[i].EspRecvPort))  # 온도 요청
-                            print("랜덤값을 요청하는중...")
+                        elif msg == "m":
+                            macron[i].kart.socket.sendto("ahrs".encode(), (macron[i].kart.Ip, macron[i].kart.Rev_port))  # ahrs값
+                            print("ahrs값을 요청하는중...")
+
+                        elif msg == "c":
+                            macron[i].kart.socket.sendto("Color".encode(), (macron[i].kart.Ip, macron[i].kart.Rev_port))  # ahrs값
+                            print("color값을 요청하는중...")
+
+
+                        else:
+                            print(f"undefine key value: {msg}")
 
                     # ESP32에서 온도 데이터 수신 → PC2로 전달
-                    elif sock == macron[i].SockSend_esp:
-                        print(f"\nReceived Random Value from [{macron[i].Name}]ESP: {msg}")
-                        macron[i].SockSend_UserPc.sendto(msg.encode(), (macron[i].PcIp, macron[i].PcSendPort))  # PC2로 온도 전송
-                        print(f"Sent Random Value to PC2: {msg}")
+                    elif sock == macron[i].kart.socket:
+                        print(f"\nReceived from [{macron[i].kart.Name}]ESP: {msg}")
+
+                        if "[ahrs]" in msg:
+                            msg=msg[6:]
+                            macron[i].player.socket.sendto(msg.encode(), (macron[i].player.Ip, macron[i].player.Rev_port))  
+                            print(f"Sent ahrs Value to PC2: {msg}")
+
+                        elif "[color]" in msg:
+                            msg=msg[7:]
+                            macron[i].player.socket.sendto(msg.encode(), (macron[i].player.Ip, macron[i].player.Rev_port))  
+                            print(f"Sent color Value to PC2: {msg}")
+
+
+                        
                 except:
+                    print("목표가 격파됨")
                     pass
 
     except KeyboardInterrupt:
