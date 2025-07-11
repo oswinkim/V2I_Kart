aca2a045fe725f4e949c7d15e2908510c78bb172
yminder <metalhero001@gmail.com>
2025-07-12 02:03:11 +0900

    #25 이미지 소스가 없을 경우 프로그램 전체가 실행되지 않는 문제 해결

--- PC_DUO_User/PC_DUO_User.py
+++ PC_DUO_User/PC_DUO_User.py
@@ -7,22 +7,21 @@ import time
 # 네트워크 설정
 MY_IP = socket.gethostbyname(socket.gethostname())  # 모든 네트워크 인터페이스에서 수신
 # ESPcam 영상 스트리밍 경로 설정
-# vid_path = "http://192.168.43.21:81/stream"
-vid_path=0  #자체카메라
+vid_path = 0#"http://192.168.0.12:81/stream"
 # PC1 통신 설정
-PC1_IP = "192.168.191.53"  # car
+PC1_IP = "192.168.0.7"  # car
 PC1_PORT = 5005            # PC1이 키 입력을 받을 포트
 PC2_RECV_PORT = 5006       # PC1에서 데이터를 받을 포트
 sock_send = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock_send.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 65536)  # 송신 버퍼 크기 증가
 sock_recv = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock_recv.bind(("", PC2_RECV_PORT))  # 온도 데이터 수신 소켓
+sock_recv.bind(("", PC2_RECV_PORT))  # 온도 데이터 수c신 소켓
 sock_recv.settimeout(0.0)  # Non-blocking 모드 (딜레이 제거)
 
 # 키 입력 상태
-keys = np.array([False] * 4)
-prev_keys = np.array([False] * 4)  # 이전 키 상태 저장
-key_map = {'f': 0, 'o': 1, 't': 2}
+keys = np.array([False] * 9)
+prev_keys = np.array([False] * 9)  # 이전 키 상태 저장
+key_map = {'w': 0, 'a': 1, 's': 2, 'd': 3, 'i': 4}
 ov_color = (220, 200, 20)
 arrowKey = np.array([
     [[200, 285], [150, 200], [250, 200]],   # ↓
@@ -45,21 +44,34 @@ def receive_data():
 
 # 키 입력 감지 및 전송 (중복 전송 방지)
 def send_key_input():
-    try:
-        key = keyboard.read_key()  # 사용자가 누른 키를 읽음
-        print("send to PC1:", key)
-        sock_send.sendto(key.encode(), (PC1_IP, PC1_PORT))
-    except OSError as e:
-        print(f"PC1 Send Error: {e}")
+    if keyboard.is_pressed('esc'):
+        return False
+    for key, index in key_map.items():
+        keys[index] = keyboard.is_pressed(key)
+        # 키 상태 변경 시 전송
+        if keys[index] != prev_keys[index]:
+            prev_keys[index] = keys[index]
+            if keys[index]:  # 키가 눌렸을 때만 전송
+                try:
+                    #sock_send.sendto(str(index).encode(), (PC1_IP, PC1_PORT))   # 입력된 키에 해당하는 인덱스를 전달
+                    sock_send.sendto(key.encode(), (PC1_IP, PC1_PORT))          # 입력된 키를 전달
+                    print("send to PC1: ",str(key))
+                except OSError as e:
+                    print(f"PC1 Send Error: {e}")
 
-
-
+img_size = [1152, 864]
 print(sock_recv.getsockname())
-img = np.empty((240, 320, 3), dtype=np.uint8)     
-#cap = cv2.VideoCapture(vid_path)
+img = np.empty((img_size[1], img_size[0], 3), dtype=np.uint8)     
+cap = cv2.VideoCapture(vid_path)
+
+win_name = "ESP32-CAM Stream"
+cv2.namedWindow(win_name, cv2.WINDOW_AUTOSIZE)
+cv2.resizeWindow(win_name, img_size[1], img_size[0])
 
 #연산컴퓨터와 연결
+'''
 while 1:
+    print("try to connect...")
     try:
         data, _ = sock_recv.recvfrom(1024)
         message = data.decode().strip()
@@ -73,39 +85,35 @@ while 1:
         pass  # 데이터가 없으면 넘어감
     except UnicodeDecodeError:
         print("Error: Received data could not be decoded.")  # 디코딩 오류 방지
-
+#'''
 
 # 메인 루프
 while True:
-
-    try:
-        receive_data()
-        send_key_input()       # 키 입력 처리
-
-    except:
-        pass
+    if False == send_key_input():       # 키 입력 처리
+        break
     #영상 읽어오기
-    '''  
-    # frame, img = cap.read()
+    frame, img = cap.read()
     #읽어오는 동영상 위치가 잘못되었으면 종료
-      
-    if not cap.isOpened():
-        print("file open failed! ([T^T])")
-        break
-    #프레임이 없다면 종료
-    elif not frame:
-        print("frame over!  ['-']>")
-        break
+    if not cap: # ret이 False인 경우 (프레임 읽기 실패)
+        print("Failed to grab frame or stream ended. ([T^T])")
+        cap.release()
+        cap = cv2.VideoCapture(vid_path)
+        if not cap.isOpened():
+            print("Could not re-open video stream.([T^T])")
+            break
+        else:
+            print("Reconnected to stream.\[+_+]/")
+        break # 프레임 읽기 실패 시 루프 종료
 
+    if not frame:
+        img = np.empty((img_size[1], img_size[0], 3), dtype=np.uint8)
+        #print("Received an empty image frame. ['-']>") # img가 None인 경우
 
-    cv2.resize(img, dsize=(img.shape[1] * 2, img.shape[0] * 2), interpolation=cv2.INTER_AREA)
-    # 화면에 출력
-    cv2.imshow("ESP32-CAM Stream", img)
-  
-    #time.sleep(1)  # 너무 빠른 업데이트 방지
-    '''
+    img = cv2.resize(img, dsize=(img_size[1], int(img_size[1]*(img.shape[0]/img.shape[1]))), interpolation=cv2.INTER_AREA)
+    cv2.imshow(win_name, img)
+
     if cv2.waitKey(1) & 0xFF == 27:  # esc 키를 누르면 종료
-        break 
+        break   
 
 # 리소스 정리
 sock_recv.close()
