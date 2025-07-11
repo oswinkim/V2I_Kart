37f549589f0e42e4aa9f4fc714d0e837a3043d6b
yminder <metalhero001@gmail.com>
2025-07-12 03:10:02 +0900

    #25 키 입력 가시성 피드백 추가 및 로직 최적화(최소 인터벌, 동시 입력 제한 등)(전송 형식은 이전과 동일)

--- PC_DUO_User/PC_DUO_User.py
+++ PC_DUO_User/PC_DUO_User.py
@@ -7,7 +7,9 @@ import time
 # 네트워크 설정
 MY_IP = socket.gethostbyname(socket.gethostname())  # 모든 네트워크 인터페이스에서 수신
 # ESPcam 영상 스트리밍 경로 설정
-vid_path = 0#"http://192.168.0.12:81/stream"
+ESP32CAM_IP = "192.168.0.12"
+ESP32CAM_PORT = 81
+
 # PC1 통신 설정
 PC1_IP = "192.168.0.7"  # car
 PC1_PORT = 5005            # PC1이 키 입력을 받을 포트
@@ -19,16 +21,45 @@ sock_recv.bind(("", PC2_RECV_PORT))  # 온도 데이터 수c신 소켓
 sock_recv.settimeout(0.0)  # Non-blocking 모드 (딜레이 제거)
 
 # 키 입력 상태
-keys = np.array([False] * 9)
-prev_keys = np.array([False] * 9)  # 이전 키 상태 저장
-key_map = {'w': 0, 'a': 1, 's': 2, 'd': 3, 'i': 4}
+keys = np.array([False] * 5)
+prev_keys = np.array([False] * 5)  # 이전 키 상태 저장
+'''
+key_map = {
+            'up': (0, 'down'),
+            'down': (1, 'up'),
+            'left': (2, 'right'),
+            'right': (3, 'left'),
+            'i': (4, 'i')
+        }
+'''
+key_map = {
+            'w': (0, 's'),
+            's': (1, 'w'),
+            'a': (2, 'd'),
+            'd': (3, 'a'),
+            'i': (4, 'i')
+        }
+#'''
+#UI 관련
 ov_color = (220, 200, 20)
-arrowKey = np.array([
-    [[200, 285], [150, 200], [250, 200]],   # ↓
-    [[200, 100], [150, 185], [250, 185]],  # ↑
-    [[25, 200], [100, 250], [100, 150]],   # ←
-    [[385, 200], [300, 250], [300, 150]]   # →
-], dtype=np.int32)
+arrows = np.array([ 
+    [[40, 20], [30, 37], [50, 37]],
+    [[40, 57], [30, 40], [50, 40]],
+    [[5, 40],  [20, 50], [20, 30]], 
+    [[77, 40], [60, 50], [60, 30]] ], dtype=np.int32)
+center = (-200, -150)
+scale = 3
+arrows =  (arrows - center) * scale + center
+
+vid_path = f"http://{ESP32CAM_IP}:{ESP32CAM_PORT}/stream"
+cap = cv2.VideoCapture(vid_path)
+
+img_size = [1152, 864]
+img = np.empty((img_size[1], img_size[0], 3), dtype=np.uint8)     
+
+win_name = " "
+cv2.namedWindow(win_name, cv2.WINDOW_AUTOSIZE)
+cv2.resizeWindow(win_name, img_size[1], img_size[0])
 
 # 데이터 수신 함수
 def receive_data():
@@ -42,55 +73,87 @@ def receive_data():
     except UnicodeDecodeError:
         print("Error: Received data could not be decoded.")  # 디코딩 오류 방지
 
-# 키 입력 감지 및 전송 (중복 전송 방지)
-def send_key_input():
-    if keyboard.is_pressed('esc'):
-        return False
-    for key, index in key_map.items():
-        keys[index] = keyboard.is_pressed(key)
-        # 키 상태 변경 시 전송
-        if keys[index] != prev_keys[index]:
-            prev_keys[index] = keys[index]
-            if keys[index]:  # 키가 눌렸을 때만 전송
-                try:
-                    #sock_send.sendto(str(index).encode(), (PC1_IP, PC1_PORT))   # 입력된 키에 해당하는 인덱스를 전달
-                    sock_send.sendto(key.encode(), (PC1_IP, PC1_PORT))          # 입력된 키를 전달
-                    print("send to PC1: ",str(key))
-                except OSError as e:
-                    print(f"PC1 Send Error: {e}")
+last_sent = time.time()
+interval = 0.15
+auto_send_count = 0
+auto_send_max = 3
+message = str("eee").encode()
+current_message = str("eee").encode()
+def auto_send():
+    global last_sent
+    global message
+    global auto_send_count
+    global auto_send_max
+    global current_message
+    if auto_send_max >= auto_send_count:
+        if time.time() - last_sent > interval:
+            send()
+            last_sent = time.time()
+            auto_send_count += 1
+    if message != current_message:
+        auto_send_count = 0
+        current_message = message
 
-img_size = [1152, 864]
-print(sock_recv.getsockname())
-img = np.empty((img_size[1], img_size[0], 3), dtype=np.uint8)     
-cap = cv2.VideoCapture(vid_path)
-
-win_name = "ESP32-CAM Stream"
-cv2.namedWindow(win_name, cv2.WINDOW_AUTOSIZE)
-cv2.resizeWindow(win_name, img_size[1], img_size[0])
-
-#연산컴퓨터와 연결
-'''
-while 1:
-    print("try to connect...")
+def send():
+    global message
     try:
-        data, _ = sock_recv.recvfrom(1024)
-        message = data.decode().strip()
-        if message:
-            print(f"Received data from PC1: {message}")
-            if "success" in message:
-                print("연산컴퓨터와 연결 성공")
-                break
-            sock_send.sendto(message.encode(), (PC1_IP, PC1_PORT))
-    except BlockingIOError:
-        pass  # 데이터가 없으면 넘어감
-    except UnicodeDecodeError:
-        print("Error: Received data could not be decoded.")  # 디코딩 오류 방지
-#'''
+        sock_send.sendto(message, (PC1_IP, PC1_PORT))   # 입력된 키에 해당하는 인덱스를 전달         
+        print("send to PC1: ",message)
+    except OSError as e:
+        print(f"PC1 Send Error: {e} (try to send: {message})")
 
+# 키 입력 감지 및 전송 (중복 전송 방지)
+def send_key_input(event):
+    global message
+    key = event.name
+    if key in key_map:
+        idx, opposite = key_map[key]
+        if event.event_type == 'down' and not keys[idx]:
+            keys[idx] = True
+            keys[key_map[opposite][0]] = False
+            message = str(key).encode() # 입력된 키를 전달
+            #message = str(index).encode() # 입력된 키에 해당하는 인덱스를 전달
+            #message = str(keys[:4]).encode()
+            send()
+        elif event.event_type == 'up' and keys[idx]:
+            keys[idx] = False
+            message = str(key).encode() # 입력된 키를 전달
+            #message = str(index).encode() # 입력된 키에 해당하는 인덱스를 전달
+            #message = str(keys[:4]).encode()
+            send()
+
+def hand_shake_with_operator():
+    #연산컴퓨터와 연결
+    while 1:
+        print("try to connect...")
+        try:
+            data, _ = sock_recv.recvfrom(1024)
+            message = data.decode().strip()
+            if message:
+                print(f"Received data from PC1: {message}")
+                if "success" in message:
+                    print("연산컴퓨터와 연결 성공")
+                    break
+                sock_send.sendto(message.encode(), (PC1_IP, PC1_PORT))
+        except BlockingIOError:
+            pass  # 데이터가 없으면 넘어감
+        except UnicodeDecodeError:
+            print("Error: Received data could not be decoded.")  # 디코딩 오류 방지
+
+# 리소스 정리
+def cleanup():
+    sock_recv.close()
+    sock_send.close()
+    cv2.destroyAllWindows()
+    print("shut down properly. [^-^]/")
+
+hand_shake_with_operator()
+print(f"MY_IP: {sock_recv.getsockname()}")
+
+keyboard.hook(send_key_input)
 # 메인 루프
 while True:
-    if False == send_key_input():       # 키 입력 처리
-        break
+    auto_send()
     #영상 읽어오기
     frame, img = cap.read()
     #읽어오는 동영상 위치가 잘못되었으면 종료
@@ -109,13 +172,15 @@ while True:
         img = np.empty((img_size[1], img_size[0], 3), dtype=np.uint8)
         #print("Received an empty image frame. ['-']>") # img가 None인 경우
 
+    for i, arrow in enumerate(arrows):
+        cv2.polylines(img, [arrow], True, (220, 200, 20), 3)
+        if keys[i]:
+            cv2.fillPoly(img, [arrow], (220, 200, 20), cv2.LINE_AA)
     img = cv2.resize(img, dsize=(img_size[1], int(img_size[1]*(img.shape[0]/img.shape[1]))), interpolation=cv2.INTER_AREA)
     cv2.imshow(win_name, img)
+    cv2.waitKey(1)
 
-    if cv2.waitKey(1) & 0xFF == 27:  # esc 키를 누르면 종료
-        break   
+    if keyboard.is_pressed('esc'):
+        break
 
-# 리소스 정리
-sock_recv.close()
-sock_send.close()
-cv2.destroyAllWindows()
+cleanup()
